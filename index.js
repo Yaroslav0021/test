d2ea534b771971820fe37d17d18abf211d99b285
yaroslav0021 <yaroslavkondratev0021@gmail.com>
2024-07-12 20:06:34 +0300

    Fitst commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = "Lets get started" ;
+});
+
